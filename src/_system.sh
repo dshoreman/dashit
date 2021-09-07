6597f7cc1a4d1e712cd7e2a0749d2f1c6e1439f1
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-09-07 23:07:10 +0100

    Add (unavailable) option for preparing host system

--- src/_system.sh
+++ src/_system.sh
@@ -1,3 +1,29 @@
 install_arch() {
     unavailable
 }
+
+prepare_host() {
+    unavailable
+
+    update_host_packages
+    install_arch_scripts
+    update_mirrorlist
+}
+
+install_arch_scripts() {
+    # If it's a manjaro install...
+    # pacman-key --populate archlinux     # May or may not need this, we'll see.
+
+    echo "Downloading Arch install scripts..."
+    pacman -Syy arch-install-scripts     # Installs packages like pacstrap, arch-chroot etc
+}
+
+update_host_packages() {
+    echo "Updating host system..."
+    pacman -Syyu
+}
+
+update_mirrorlist() {
+    echo "Fetching filtered mirrorlist..."
+    curl "https://archlinux.org/mirrorlist/?country=GB&protocol=https&ip_version=4&use_mirror_status=on" > /etc/pacman.d/mirrorlist
+}
--- src/_welcome.sh
+++ src/_welcome.sh
@@ -16,6 +16,8 @@ welcome_screen() {
         2)
             provision_partition "$@" ;;
         3)
+            prepare_host ;;
+        4)
             install_arch ;;
         q)
             exit 0
@@ -28,7 +30,8 @@ print_menu() {
     echo
     echo " [ 1] Partition and format disk"
     echo " [ 2] Create btrfs subvolumes"
-    echo " [ 3] Install Arch Linux"
+    echo " [ 3] Prepare host system"
+    echo " [ 4] Install Arch Linux"
     echo " [ q] Quit"
     echo
 
